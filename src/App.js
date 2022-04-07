bdaa772141842046a4954e77efaf07d7d79eddab
esmeraldaa01 <lameesmeralda7@gmail.com>
2022-04-07 15:07:48 +0200

    m

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
 import './App.css';
 import {Input ,Button} from "antd";
-import woman  from './assests/woman.jpg'
+import jordanCardo  from './assests/jordanCardo.jpg'
 import {useState} from "react";
 
 const  App = () => {
-    const [count , setCount] = useState(12);
-    const [password , setPassWord ]  = useState('')
-
+    const [count , setCount] = useState(80);
+    const [password , setPassWord ]  = useState('');
 
   return (
       <div className='background'>
-           <img style={{width:'100vw', height:'100vh',filter: `blur(${count}px) ` }} src={woman} />
+           <img style={{width:'100vw', height:'100vh',filter: `blur(${count - ((count - 12) + password.length)}px) ` }} src={jordanCardo} />
 
         <div className='form-card'>
+            <div style={{marginBottom:'10px'}}>
             <p className='header'>Image Password Strength</p>
-            <p className='paragraph'>Change the password to see the effect</p>
+              <small className='paragraph'>Change the password to see the effect</small>
+            </div>
                 <label className='label'>Email: </label>
-                <Input label="name"
-                       style={{padding: '6px'}}
+                <Input style={{padding: '6px'}}
                        placeholder="Enter email"
                 />
             <label className='label'>Password: </label>
